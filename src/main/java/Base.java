835d1f8d70dc023d93c2c30371bee572aff6beed
zhulina <zhulina@nsd.ru>
2017-06-28 00:36:12 +0300

    Задание 2*

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -2,17 +2,68 @@ import java.util.Scanner;
 
 /**
  * Created by Татьяна on 24.06.2017.
+ * @author Татьяна
+ * @see #calculator()
  */
 public class Base {
 
+    private static Scanner scanner = new Scanner(System.in); // Переменная класса для считывания ввода их консоли
+
+    /**
+     * Начало программы
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        Float firstNum = scanner.nextFloat();
-        System.out.println("Enter the second number:");
-        Float secondNum = scanner.nextFloat();
-        Float result = firstNum + secondNum;
-        System.out.printf("Sum is: %.4f\n", result);
-        scanner.close();
+        // Создаем объект класса Base
+        Base programm = new Base();
+        programm.calculator();
+    }
+
+    /**
+     * Метод для работы калькулятора
+     */
+    private void calculator() {
+        // Вводим данные
+        Float firstNum = Float.parseFloat(input("first number").replaceAll(",", "\\."));
+        Float secondNum = Float.parseFloat(input("second number").replaceAll(",", "\\."));
+        String operation = input("operation");
+        // Выводим результат
+        System.out.printf("Result is: %.4f\n", counting(firstNum, secondNum, operation));
+    }
+
+    /**
+     * Метод ввода из консоли
+     *
+     * @param message String уточнение ввода чего именно
+     * @return String полученная строка
+     */
+    private String input(String message) {
+        System.out.println(String.format("Enter the %s:", message));
+        return scanner.next();
+    }
+
+    /**
+     * Метод вычисления операции над двумя числами
+     *
+     * @param firstNum  Float первое число
+     * @param secondNum Float второе число
+     * @param operation String символ операции, + - * /
+     * @return Float полученный результат операции
+     */
+    private Float counting(Float firstNum, Float secondNum, String operation) {
+        switch (operation) {
+            case "+":
+                return firstNum + secondNum;
+            case "-":
+                return firstNum - secondNum;
+            case "*":
+                return firstNum * secondNum;
+            case "/":
+                return firstNum / secondNum;
+            default:
+                System.err.println("Неопознан тип операции");
+                return -1f;
+        }
     }
 }
